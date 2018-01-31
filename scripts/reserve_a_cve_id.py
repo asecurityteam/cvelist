c30566f855a026f7a3efa5a05f1b2b3d21ae131a
David Black <dblack@atlassian.com>
2018-01-31 16:46:16 +1100

    SECENG-1125: add a script to reserve a cve id.
    Signed-off-by: David Black <dblack@atlassian.com>

---
+++ scripts/reserve_a_cve_id.py
@@ -0,0 +1,93 @@
+#!/usr/bin/python
+import json
+import os
+import re
+import subprocess
+import sys
+
+from . import utils
+
+
+def get_used_cve_id_from_git_branches():
+    """ yields cve ids from git branches of this repository. """
+    cve_pattern = re.compile('(CVE-\d+-\d+)')
+    git_command = [
+        'git', 'branch', '-a', '--list', '*CVE*',
+        '--format', '%(refname:short)']
+    cve_branch_exe = subprocess.Popen(git_command, stdout=subprocess.PIPE)
+    out, err = cve_branch_exe.communicate()
+
+    for line in out.decode().split(os.linesep):
+        line = line.strip()
+        match = cve_pattern.search(line)
+        if match is None:
+            continue
+        cve_id = match.group()
+        yield cve_id
+
+
+def get_free_cve_ids(used_cve_ids, files):
+    """ yields free (reserved) cve ids and matching cve id file path. """
+    for a_file in files:
+        existing_json_data = utils.get_info_from_cve_json_file(a_file)
+        state = utils.get_state_from_cve_json(existing_json_data)
+        if state != 'RESERVED':
+            continue
+        cve_id = existing_json_data['CVE_data_meta']['ID']
+        if cve_id not in used_cve_ids:
+            yield (cve_id, a_file)
+
+
+def load_template(name='base.json'):
+    """ returns the loaded template file. """
+    template_dir = os.path.join(
+        utils.get_internal_cvelist_location(),
+        'templates')
+    with open(os.path.join(template_dir, name), 'r') as f:
+        return json.load(f)
+
+
+def reserve_cve_id(cve_id, filepath):
+    """ setups up the cve id for reservation. """
+    a_dir = os.path.dirname(filepath)
+    dest_dir = a_dir
+    split_dir = os.path.split(a_dir)
+    if split_dir[-1] == 'reserved':
+        dest_dir = os.path.join(*split_dir[:-1])
+    dest_file_path = os.path.join(dest_dir, os.path.basename(filepath))
+    output_json = load_template()
+    output_json['CVE_data_meta']['ID'] = cve_id
+    with open(dest_file_path, 'w') as f:
+        json.dump(output_json, f, indent=4)
+    os.unlink(filepath)
+
+
+def reserve_cve_id_in_git(cve_id):
+    """ reserves the cve id in git by creating a branch for it. """
+    subprocess.check_call(['git', 'checkout', '-q', '-b', cve_id])
+    subprocess.check_call(['git', 'push', '-q', 'origin', cve_id])
+
+
+def main():
+    if len(sys.argv) <= 1:
+        sys.exit('Please specify a year.')
+    year = int(sys.argv[1])
+    used_cve_ids = set(get_used_cve_id_from_git_branches())
+    year_allocation = os.path.join(
+        utils.get_internal_cvelist_location(), str(year))
+    files = utils.get_file_listing_for_path(year_allocation, ['*.json'])
+    free_cve_ids = list(get_free_cve_ids(used_cve_ids, files))
+    if not free_cve_ids:
+        sys.exit('No free CVE ids for %s. Request more from mitre.' % (
+            year))
+    cve_id_to_reserve, cve_id_f_path = free_cve_ids[0]
+    print('Reserving', cve_id_to_reserve)
+    reserve_cve_id(cve_id_to_reserve, cve_id_f_path)
+    print('Creating a git branch for', cve_id_to_reserve)
+    reserve_cve_id_in_git(cve_id_to_reserve)
+    print('\nPlease modify %s as desired and then commit & '
+          'push your changes.' % (cve_id_f_path))
+
+
+if __name__ == '__main__':
+    main()
